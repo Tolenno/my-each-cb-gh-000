88eb583fc2dddd45f1b086318a977ed75a65c0c7
Matt Hough <matt@hough.tv>
2017-09-26 07:16:09 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,11 @@
 def my_each(array)
   if block_given?
-  index = 0
-  while index < array.size
-    yield array[index]
-    index += 1
-  end
-  array
+    index = 0
+    while index < array.size
+      yield array[index]
+      index += 1
+    end
+    array
+  else
+    "Game over man, Game over!"
 end
-
